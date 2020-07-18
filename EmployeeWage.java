f4b657c9bf7f0d4fccd3a9499d5d4791f8d4a53b
unknown <lavanyareddy19994@gmail.com>
2020-07-18 13:37:44 +0530

    monthly wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -13,19 +13,20 @@ public class EmployeeWage {
 		int full_day_hour=8;
 		int part_time_hour=4;
 		int salary=0;
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		switch(empCheck)
+		int days_in_month=20;
+		for(int i=1;i<=days_in_month;i++)
 		{
-		    case 1:System.out.println("Employee is present for full time");
-		           salary=Wage_per_hour*full_day_hour; 
+		  int empCheck=(int)Math.floor(Math.random()*10)%3;
+		  switch(empCheck)
+		  {
+		    case 1:salary+=Wage_per_hour*full_day_hour; 
 		           break;
-		    case 2:System.out.println("Employee is present for part time");
-			       salary=Wage_per_hour*part_time_hour;
+		    case 2:salary+=Wage_per_hour*part_time_hour;
 			       break;
-			default:System.out.println("Employee is absent");
-		
+			default:salary+=0;
+		  }
 		}
-		System.out.println("Salary is:"+salary);
+		System.out.println("Salary for month is:"+salary);
 	}
 
 }
