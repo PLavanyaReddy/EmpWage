80b4dfc40c19b9f595e079f852bcc0dfe3f16d61
unknown <lavanyareddy19994@gmail.com>
2020-07-18 12:32:02 +0530

    welcome

---
+++ EmployeeWage.java
@@ -0,0 +1,13 @@
+
+public class EmployeeWage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation program");
+
+	}
+
+}
