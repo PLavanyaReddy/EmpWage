f6fee8a485f558e4ce520e211afad89aa67d1e52
unknown <lavanyareddy19994@gmail.com>
2020-07-18 13:25:57 +0530

    case statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -13,8 +13,19 @@ public class EmployeeWage {
 		int full_day_hour=8;
 		int part_time_hour=4;
 		int salary=0;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if(empCheck == IS_FULL_TIME)
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		switch(empCheck)
+		{
+		    case 1:System.out.println("Employee is present for full time");
+		           salary=Wage_per_hour*full_day_hour; 
+		           break;
+		    case 2:System.out.println("Employee is present for part time");
+			       salary=Wage_per_hour*part_time_hour;
+			       break;
+			default:System.out.println("Employee is absent");
+		
+		}
+		/*if(empCheck == IS_FULL_TIME)
 		{
 			System.out.println("Employee is present for full time");
 			salary=Wage_per_hour*full_day_hour;
@@ -28,7 +39,7 @@ public class EmployeeWage {
 		{
 			System.out.println("Employee is absent");
 			salary=0;
-		}
+		}*/
 		System.out.println("Salary is:"+salary);
 	}
 
