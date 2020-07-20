b179deb4e320e76a3258fc69897e0c9182c4e741
unknown <lavanyareddy19994@gmail.com>
2020-07-20 12:44:55 +0530

    modified

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,39 +1,45 @@
 
 public class EmployeeWage {
-
+	static final int IS_FULL_TIME=1;
+	static final int IS_PART_TIME=2;
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation program");
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
+	void wageComputation()
+	{
 		int Wage_per_hour=20;
 		int full_day_hour=8;
 		int part_time_hour=4;
 		int salary=0;
+		int totalSalary=0;
 		int days_in_month=20;
 		int working_hours=0;
 		int working_days=0;
-		while(working_hours<100 && working_days<20)
+		System.out.println("Day\tDailyWage\tTotalEmployeeHours");
+		while(working_hours<100 && working_days<days_in_month)
 		{
 		  working_days++;
 		  int empCheck=(int)Math.floor(Math.random()*10)%3;
 		  switch(empCheck)
 		  {
-		    case 1:salary+=Wage_per_hour*full_day_hour; 
-		    	   working_hours+=full_day_hour;
-		           break;
-		    case 2:salary+=Wage_per_hour*part_time_hour;
-		           working_hours+=part_time_hour;
-			       break;
-			default:salary+=0;
+		    case IS_FULL_TIME:salary=Wage_per_hour*full_day_hour; 
+		    	              working_hours+=full_day_hour;
+		                      break;
+		    case IS_PART_TIME:salary=Wage_per_hour*part_time_hour;
+		                      working_hours+=part_time_hour;
+			                  break;
+			default:salary=0;
 		  }
+		  totalSalary+=salary;
+		  System.out.println(working_days+"\t"+salary+"\t\t"+working_hours);
 		}
-		System.out.println("No.of working days:"+working_days);
-		System.out.println("No.of working hours:"+working_hours);
-		System.out.println("Salary for month is:"+salary);
+		System.out.println("Total salary of employee is:"+totalSalary);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation program");
+		EmployeeWage e1=new EmployeeWage();
+		e1.wageComputation();
 	}
 
 }
