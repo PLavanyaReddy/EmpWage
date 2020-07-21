157e8baead3a9e798eceb2e45f65a7d1d2fc8ef1
unknown <lavanyareddy19994@gmail.com>
2020-07-21 13:32:07 +0530

    modified

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,45 +1,57 @@
 
 public class EmployeeWage {
-	static final int IS_FULL_TIME=1;
-	static final int IS_PART_TIME=2;
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+    String companyName;
+	int wage_per_hour;
+	int working_days;
+    int working_hours;
 	/**
 	 * @param args
 	 */
-	void wageComputation()
+	public EmployeeWage(String companyName,int wage_per_hour,int working_days,int working_hours)
 	{
-		int Wage_per_hour=20;
+		this.companyName=companyName;
+		this.wage_per_hour=wage_per_hour;
+		this.working_days=working_days;
+		this.working_hours=working_hours;
+	}
+	int wageComputation()
+	{
+		int days=0;
 		int full_day_hour=8;
 		int part_time_hour=4;
+		int hours=0;
 		int salary=0;
 		int totalSalary=0;
-		int days_in_month=20;
-		int working_hours=0;
-		int working_days=0;
+		System.out.println("For "+companyName+":");
 		System.out.println("Day\tDailyWage\tTotalEmployeeHours");
-		while(working_hours<100 && working_days<days_in_month)
+		while(hours<working_hours && days<working_days)
 		{
-		  working_days++;
+		  days++;
 		  int empCheck=(int)Math.floor(Math.random()*10)%3;
 		  switch(empCheck)
 		  {
-		    case IS_FULL_TIME:salary=Wage_per_hour*full_day_hour; 
-		    	              working_hours+=full_day_hour;
+		    case IS_FULL_TIME:salary=wage_per_hour*full_day_hour; 
+		    	              hours+=full_day_hour;
 		                      break;
-		    case IS_PART_TIME:salary=Wage_per_hour*part_time_hour;
-		                      working_hours+=part_time_hour;
+		    case IS_PART_TIME:salary=wage_per_hour*part_time_hour;
+		                      hours+=part_time_hour;
 			                  break;
 			default:salary=0;
 		  }
 		  totalSalary+=salary;
-		  System.out.println(working_days+"\t"+salary+"\t\t"+working_hours);
+		  System.out.println(days+"\t"+salary+"\t\t"+hours);
 		}
-		System.out.println("Total salary of employee is:"+totalSalary);
+		return totalSalary;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation program");
-		EmployeeWage e1=new EmployeeWage();
-		e1.wageComputation();
+		EmployeeWage dmart=new EmployeeWage("DMART",20,20,100);
+		System.out.println("Total Emp Wage for "+dmart.companyName+" is "+dmart.wageComputation());
+		EmployeeWage reliance=new EmployeeWage("Reliance",30,25,95);
+		System.out.println("Total Emp Wage for "+reliance.companyName+" is "+reliance.wageComputation());
 	}
 
 }
